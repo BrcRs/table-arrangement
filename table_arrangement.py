9f194eabe1a47e5dad8b32f8d9611a089de7211c
Bruce Rose <brucej.rose@orange.fr>
2021-12-20 12:40:19 +0100

    Started coding the problem model

---
+++ table_arrangement.py
@@ -0,0 +1,27 @@
+class TAProblem:
+    def __init__(self):
+        self.guests = dict()
+        self.topology = dict()
+        self.constraints = dict()
+
+    def add_guest(self, name, alias):
+        self.guests[alias] = name
+        self.constraints[alias] = dict()
+
+    def add_guests(self, aliases: dict):
+        for a in aliases.keys():
+            self.add_guest(self, aliases[a], a)
+
+    def add_edge(self, first, second):
+        if first not in self.topology.keys():
+            self.topology[first] = set()
+
+        if second not in self.topology.keys():
+            self.topology[second] = set()
+        
+        self.topology[first].add(second)
+        self.topology[second].add(first)
+    
+    def add_constraint(self, guest, other, value):
+        # other could be a seat or a guest
+        self.constraints[guest][other] = value

bd57c17e8780884169fc1e928ebdc1fd83625441
Bruce Rose <brucej.rose@orange.fr>
2021-12-20 12:53:19 +0100

    Added line counting when interpreting the model file + main func

--- interpreter.py
+++ interpreter.py
@@ -1,21 +1,26 @@
 from table_arrangement import TAProblem
 import string
+import sys
+
 def load_problem(filename):
     if filename[-3:] != ".ta":
         raise NameError("The file is not a .ta file")
     f = open(filename, "r")
     problem = TAProblem()
     aliases = dict()
+    nline = 0
     # for each line:
     line = f.readline()
     while line:
         # if it begins with guests:
         if line[:len("guests:")] == "guests:":
             line = f.readline()
+            nline += 1
             # while the line begins with \t or \n:
             while line:
                 if len(line) == 0:
                     line = f.readline()
+                    nline += 1
                     continue
                 if line[0] != "\t":
                     break
@@ -32,18 +37,19 @@ def load_problem(filename):
                 
                 # check for syntax errors
                 
-                f.readline()
+                line = f.readline()
+                nline += 1
             # Add guests to problem
             problem.add_guests(aliases)
 
         # if it begins with /*:
         elif line[:2] == "/*":
             line = f.readline()
-
+            nline += 1
             # pass lines until */ is found
             while line and not "*/" in line:
                 line = f.readline() # Caution: lines with */ will be ignored?
-
+                nline += 1
         # if it begins with //:
         elif line[:2] == "//":
             # pass
@@ -52,17 +58,18 @@ def load_problem(filename):
         # if it begins with blank:
         elif line[0] == " ":
             # throw syntax error
-            raise SyntaxError()
+            raise SyntaxError("Lonely blank on line " + str(nline))
 
         # if it begins with topology:
         elif line[:len("topology:")] == "topology:":
             line = f.readline()
-
+            nline += 1
             # while the line begins by \t or \n:
             while line:
 
                 if len(line) == 0:
                     line = f.readline()
+                    nline += 1
                     continue
 
                 if line[0] != "\t":
@@ -78,12 +85,14 @@ def load_problem(filename):
         # if it begins with constraints:
         elif line[:len("constraints:")] == "constraints:":
             line = f.readline()
+            nline += 1
 
             # while the line begins by \t or \n:
             while line:
 
                 if len(line) == 0:
                     line = f.readline()
+                    nline += 1
                     continue
 
                 if line[0] != "\t":
@@ -107,12 +116,14 @@ def load_problem(filename):
         # if it begins with problem:
         elif line[:len("problem:")] == "problem:":
             line = f.readline()
+            nline += 1
 
             # while the line begins by \t or \n:
             while line:
 
                 if len(line) == 0:
                     line = f.readline()
+                    nline += 1
                     continue
 
                 if line[0] != "\t":
@@ -130,3 +141,10 @@ def load_problem(filename):
             # throw syntax error
             raise SyntaxError()
         # line = f.readline()
+    return problem
+
+def main():
+    print(sys.argv)
+    problem = load_problem(sys.argv[1])
+if __name__ == "__main__":
+    main()

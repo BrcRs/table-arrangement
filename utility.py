bb3f576faf679b18d42ffd0552aa8893ec112799
Bruce Rose <brucej.rose@orange.fr>
2021-12-20 20:23:29 +0100

    #8 Copying dicts is done recursively now

--- utility.py
+++ utility.py
@@ -16,5 +16,10 @@ def is_number(s):
 def copy_dict(some_dict):
     new_copy = dict()
     for k in some_dict.keys():
-        new_copy[k] = some_dict[k]
+        if type(some_dict[k]) == dict:
+            new_copy[k] = copy_dict(some_dict[k])
+        elif type(some_dict[k]) == list:
+            new_copy[k] = some_dict[k].copy()
+        else:
+            new_copy[k] = some_dict[k]
     return new_copy
